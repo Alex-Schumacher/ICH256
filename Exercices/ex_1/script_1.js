9464d1879822e7e7c94820bcdf2c8686f90c8d43
Schumacher <Alexandre.Schumacher@ceff.ch>
2021-08-24 11:05:27 +0200

    Exercice 1

---
+++ Exercices/ex_1/script_1.js
@@ -0,0 +1,6 @@
+$(function(){
+    $("#content").css("background-color","gray");
+    $("h1,h2").css("color","red");
+    $("p").css("color","black");
+    $(".anecdote+ p, .anecdote").css("color","white");
+});
